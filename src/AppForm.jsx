14035f5b2fbdcd0504192cc41a824864ed9c2c65
frontenddeveloper-leejeongeun <49803637+frontend-leejeongeun@users.noreply.github.com>
2024-12-19 22:24:27 +0900

    리액트 기본 개념정리

---
+++ src/AppForm.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export default function AppForm() {
+  const [form, setForm] = useState({ title: "", email: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="title">이름:</label>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={form.title}
+        onChange={handleChange}
+      />
+      <label htmlFor="email">이메일:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <button>Submit</button>
+    </form>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+//import AppTheme from "./AppTheme";
+import AppProducts from "./basic/AppProducts";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProducts />
   </React.StrictMode>
 );
 
